c171d8a939ae7ca4b29459eeb76d6c2ddc81ab16
Aimar Aareleid <aareleid@worth.systems>
2018-09-06 14:35:31 +0200

    initial

--- src/conf/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const { dbSettings, serverSettings } = require('./config');
-const db = require('./mongoose');
-
-module.exports = Object.assign({}, { dbSettings, serverSettings, db });
